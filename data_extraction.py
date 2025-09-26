f9c898fde690f17784625169aa3f4bdc38aa2fb0
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-09-26 16:48:15 +0100

    Added python script that extract that flat files from local systemAdd Python script to extract flat files from local system
    This script loops over monthly parquet files in Documents and prints them using pandas.

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,10 @@
-import requests
 import pandas as pd
-
 def data_extract():
-    url = ""
+    months = range(1, 12+1)
+    for month in months:
+        data_path = f'C:/Users/USER/Documents/yellow_tripdata_2024-{month:02d}.parquet'
+        raw_data = pd.read_parquet(data_path)
+        print(raw_data)
+
+if __name__ == "__main__":
+    data_extract()
