0cf3003fd810d7c25e56ba419cedb7ebea7ee7c4
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-10-09 08:51:30 +0100

    Added data extractiong script

--- data_extraction.py
+++ data_extraction.py
@@ -1,10 +1,18 @@
 import pandas as pd
 def data_extract():
+    raw_data = []
     months = range(1, 12+1)
+    print('Data extraction begins')
     for month in months:
         data_path = f'C:/Users/USER/Documents/yellow_tripdata_2024-{month:02d}.parquet'
-        raw_data = pd.read_parquet(data_path)
-        print(raw_data)
+        print(f'Reading file {data_path}')
+        data = pd.read_parquet(data_path)
+        raw_data.append(data)
+    #Combined raw data into a single DataFrame
+    combined_data = pd.concat(raw_data, ignore_index = False )
+    print("Data extraction completed successfully ✅.")
+    return combined_data
 
 if __name__ == "__main__":
-    data_extract()
+   df = data_extract()
+   
