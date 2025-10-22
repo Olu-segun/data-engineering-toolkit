e21439982ed8d5092b65037a96a9157a48861255
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-10-22 18:56:50 +0100

    Updated

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,8 +6,10 @@ from data_extraction import data_extract
 
 def transform_data():
 	df = data_extract()
+	df = df.dropna()
+	return df
 	
 	
-	
-if__name__==  "__main__"
-	transform_data()
+if __name__=="__main__":
+    df = transform_data()
+    print(df)
