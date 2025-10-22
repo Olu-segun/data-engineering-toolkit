22829f4751f26f4e124def3d7ec080b31700a6f2
Olukayode Olusegun <olukayodeoluseguno@gmail.com>
2025-10-22 19:40:25 +0100

    Updated

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,8 +7,7 @@ def transform_data():
 	print("Data transformation begins")
 	df = data_extract()
 	df = df.dropna()
-	print("Data transformation completed successfully ✅")
-	return df
+	print("Data transformation completed successfully ✅ ")
 	
 if __name__==  "__main__":
 	df = transform_data()
